6b7600f60603a317d32a0ac2820ac7ec39653197
Rishi Bhatia <brishi11128@gmail.com>
2026-08-22 15:38:10 +0530

    config server fixed

--- activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
@@ -15,7 +15,7 @@ public class WebClientConfig {
 
     @Bean
     public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder().baseUrl("http://USERSERVICE").build();
+        return webClientBuilder().baseUrl("http://USER-SERVICE").build();
     }
 
 }
